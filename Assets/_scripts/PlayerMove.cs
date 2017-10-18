f4ef852c03847ec24c244f6ef47623a447eaf405
Josh <joshuajw@hawaii.edu>
2017-10-18 12:59:05 -1000

    Added auto movement and player

---
+++ Assets/_scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(1f * Time.deltaTime, 0f, 0f);
+    }
+}
