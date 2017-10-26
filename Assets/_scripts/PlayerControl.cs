eb736c7811aa94a9325bef598ba703d53c95bdae
Kyle Balisacan <kylebali@hawaii.edu>
2017-10-25 14:18:09 -1000

    Controls
    Added basic jump; collision problems

---
+++ Assets/_scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+	public float jump;
+	float moveVelocity;
+	bool ground = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
+		}
+	}
+}
