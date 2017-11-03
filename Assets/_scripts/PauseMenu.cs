23e71df7f4ca75b6a116f6ef90bfcd697449f2f0
Josh <joshuajw@hawaii.edu>
2017-11-02 19:49:34 -1000

    Added Pause Menu
    press esc to open menu

---
+++ Assets/_scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip clip;
+    public GameObject PauseUI;
+    private bool paused = false;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);    
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+
+        if(paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+            audioSource.Pause();
+        }
+        else if(!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+            audioSource.UnPause();
+        }
+
+    }
+
+}
