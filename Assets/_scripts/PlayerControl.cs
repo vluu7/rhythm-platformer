54e290bf29c68ac46e74c480ab5e55648bf499d6
Kyle Balisacan <kylebali@hawaii.edu>
2017-11-06 07:39:35 -1000

    Added touch gestures

--- Assets/_scripts/PlayerControl.cs
+++ Assets/_scripts/PlayerControl.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerControl : MonoBehaviour {
+	
+	private float checkTouchHold = 0.8f; 
+	private float touchHoldTime = 0;
+	private float minSwipeDistance = 50.0f;
+	private float maxSwipeTime = 0.5f;
+	private float fingerSwipeTime = 0.0f;
+	private Vector2 fingerStartPos = Vector2.zero;
+	private bool swipe = false;
 
 	public float speed;
 	public float jump;
 	float moveVelocity;
-	bool ground = false;
+	// bool ground = false;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +28,7 @@ public class PlayerControl : MonoBehaviour {
         /*if (Input.GetMouseButtonDown (0)) {
             //GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
             transform.position = new Vector3(0, 4.12f, 0);
-        }*/
+        }
 
         if (Input.GetKeyDown(KeyCode.A))
             transform.position = new Vector3(transform.position.x, 6.42f, -7);
@@ -29,6 +38,69 @@ public class PlayerControl : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.D))
             transform.position = new Vector3(transform.position.x, -2.42f, -7);
+		*/
 
-    }
+		if (Input.touchCount > 0) {
+			
+			touchHoldTime += Input.GetTouch(0).deltaTime;
+
+			foreach (Touch touch in Input.touches) {
+
+				switch (touch.phase) {
+				case TouchPhase.Began:
+					swipe = true;
+					fingerSwipeTime = Time.time;
+					fingerStartPos = touch.position;
+					break;
+				
+				case TouchPhase.Canceled:
+					swipe = false;
+					break;
+				
+				case TouchPhase.Ended:
+					float gestureTime = Time.time - fingerSwipeTime;
+					float gestureDist = (touch.position - fingerStartPos).magnitude;
+
+					if (swipe && gestureTime < maxSwipeTime && gestureDist > minSwipeDistance) {
+						Vector2 direction = touch.position - fingerStartPos;
+						Vector2 swipeType = Vector2.zero;
+
+						if (Mathf.Abs (direction.x) > Mathf.Abs (direction.y)) { //if swipe is horizontal
+							swipeType = Vector2.right * Mathf.Sign (direction.x);
+						} else { //if swipe is vertical
+							swipeType = Vector2.up * Mathf.Sign(direction.y);
+						}
+
+						if (swipeType.x != 0.0f) {
+							if (swipeType.x > 0.0f) {
+								//move right
+								transform.position = new Vector3(transform.position.x, 6.42f, -7);
+							} else {
+								//move left
+							}
+						}
+
+						if (swipeType.y != 0.0f ) {
+							if (swipeType.y > 0.0f) {
+								//move up
+								transform.position = new Vector3(transform.position.x, 2.00f, -7);
+							} else {
+								//move down
+							}
+						}
+
+						if (touchHoldTime > checkTouchHold) {
+							//touch and hold gesture
+							transform.position = new Vector3(transform.position.x, -2.42f, -7);
+						}
+
+					}
+					touchHoldTime = 0;
+					break;
+				} 
+
+			}
+		}
+	}
 }
+
