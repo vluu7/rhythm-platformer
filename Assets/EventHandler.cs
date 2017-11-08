0925236ecfca0ca170ba4c9b62b4d8fdea552411
Vincent Luu <vluu7@hawaii.edu>
2017-11-08 13:50:20 -1000

    First Try of Koreography Events and Scripts

---
+++ Assets/EventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+/*
+ * First Try of Koreographer Event Handler Script.
+ */
+
+public class EventHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    void FireEventDebugLog(KoreographyEvent koreoEvent) 
+    {
+        Debug.Log("Koreography Event Fired.");
+    }
+
+
+
+}
